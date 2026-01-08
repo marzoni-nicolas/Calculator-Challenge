ee8a75e476b4d7577afb7ab37b5cefcb5f896a2c
Nicolás Marzoni <marzoni.nicolas@belonghome.com>
2026-01-08 16:30:17 -0300

    Initial project commit, including simple addition of two numbers.

---
+++ Calculator-Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator_Challenge.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection()
+    .AddSingleton<ICalculator, Calculator>()
+    .BuildServiceProvider();
+
+var calculator = services.GetRequiredService<ICalculator>();
+
+// Expect the expression as the first argument
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: dotnet run -- \"3,4\"");
+    return;
+}
+
+var result = calculator.Add(args[0]);
+Console.WriteLine($"Result: {result}");
---
+++ Calculator-Challenge/Services/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using Calculator_Challenge.Exceptions;
+
+namespace Calculator_Challenge.Services;
+
+public interface ICalculator
+{
+    int Add(string input);
+}
+
+public sealed class Calculator : ICalculator
+{
+    public int Add(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        // Keeping this simple as first iteration. "Add" method could accept two arguments a int and b int, so we just do a+b.
+        var parts = input.Split(',');
+
+        if (parts.Length > 2)
+        {
+            throw new TooManyNumbersException(parts.Length);
+        }
+
+        return parts.Sum(ParseNumber);
+    }
+
+    /// <summary>
+    /// Parse string into a number. Returns the numeric representation of the string or 0 if the string is an invalid int.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    private static int ParseNumber(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return 0;
+        }
+
+        return int.TryParse(value, out var number)
+            ? number
+            : 0;
+    }
+}
