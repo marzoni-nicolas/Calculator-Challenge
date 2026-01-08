098f93f69b38bbf1f1d0d2d6d4faaebbecdb3c44
Nicolás Marzoni <marzoni.nicolas@belonghome.com>
2026-01-08 16:30:17 -0300

    Add project files.

---
+++ Calculator-Challenge.Tests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using Services = Calculator_Challenge.Services;
+using Calculator_Challenge.Exceptions;
+using FluentAssertions;
+using Xunit;
+
+namespace Calculator.Tests;
+
+public class CalculatorTests
+{
+    private readonly Services.ICalculator _calculator = new Services.Calculator();
+
+    [Theory]
+    [InlineData("20", 20)]
+    [InlineData("1,5000", 5001)]
+    [InlineData("4,-3", 1)]
+    [InlineData("", 0)]
+    [InlineData(",", 0)]
+    [InlineData("5,tytyt", 5)]
+    public void Add_ValidInputs_ReturnsExpectedResult(string input, int expected)
+    {
+        var result = _calculator.Add(input);
+
+        result.Should().Be(expected);
+    }
+
+    [Fact]
+    public void Add_MoreThanTwoNumbers_ThrowsException()
+    {
+        var act = () => _calculator.Add("1,2,3");
+
+        act.Should()
+            .Throw<TooManyNumbersException>()
+            .WithMessage("*maximum of 2 numbers*");
+    }
+}
---
+++ Calculator-Challenge/Exceptions/TooManyNumbersException.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator_Challenge.Exceptions;
+
+public sealed class TooManyNumbersException : Exception
+{
+    public TooManyNumbersException(int count)
+        : base($"A maximum of 2 numbers is allowed. Received {count}.")
+    {
+    }
+}
