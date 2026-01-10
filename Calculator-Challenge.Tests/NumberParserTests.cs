bdb85113a7b85f3629ae33791d2aa4f7faf4fc46
Nicolas Matias Marzoni <110948389+marzoni-nicolas@users.noreply.github.com>
2026-01-10 11:06:40 -0300

    Bugfix/Passing comma as a custom delimiter fails (#6)
    Fixing parser to NOT use default delimiters when custom delimiter header is present.
    Adding simple integration tests to speed up end-to-end tests from terminal.

--- Calculator-Challenge.Tests/NumberParserTests.cs
+++ Calculator-Challenge.Tests/NumberParserTests.cs
@@ -27,6 +27,7 @@ public class NumberParserTests
     [InlineData("1,\n2", new[] { 1, 0, 2 })]
     [InlineData("\n", new[] { 0 })]
     [InlineData("4\n-3", new[] { 4, -3 })]
+    [InlineData("4,-3", new[] { 4, -3 })]
     public void Parse_SupportsNewlineAndCommaDelimiters(string input, int[] expected)
     {
         var result = _parser.Parse(input);
@@ -34,14 +35,16 @@ public class NumberParserTests
         result.Should().Equal(expected);
     }
 
-    [Fact]
-    public void Parse_SupportsSingleCharacterCustomDelimiter()
+    [Theory]
+    [InlineData("//#\n2#5", new[] { 2, 5 })]
+    [InlineData("//,\n3,4", new[] { 3, 4 })]
+    public void Parse_SupportsSingleCharacterCustomDelimter(string input, int[] expected)
     {
         var parser = new NumberParser();
 
-        var result = parser.Parse("//#\n2#5");
+        var result = parser.Parse(input);
 
-        result.Should().Equal(2, 5);
+        result.Should().Equal(expected);
     }
 
     [Fact]
@@ -57,6 +60,7 @@ public class NumberParserTests
     [Theory]
     [InlineData("//[***]\n11***22***33", new[] { 11, 22, 33 })]
     [InlineData("//[&]\n11&22&33", new[] { 11, 22, 33 })]
+    [InlineData("//[,]\n11,22,33", new[] { 11, 22, 33 })]
     public void Parse_SupportsCustomDelimiterOfAnyLength(string input, int[] expected)
     {
         var parser = new NumberParser();
@@ -69,6 +73,7 @@ public class NumberParserTests
     [Theory]
     [InlineData("//[***]\n***", new[] { 0 ,0 })]
     [InlineData("//[##]\n####", new[] { 0, 0, 0 })]
+    [InlineData("//[,,]\n,,", new[] { 0, 0 })]
     public void Parse_SupportsCustomDelimiterOfAnyLength_WithoutNumbers(string input, int[] expected)
     {
         var parser = new NumberParser();
